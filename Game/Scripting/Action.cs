dc53e26ec93f64398a3791bcbdc6852f4459c3a0
SherwoJo <jordan.sherwoodfam@gmail.com>
2022-07-12 11:00:17 -0600

    Completed Director Class and Related Classes: Cast, Score, Action, and Script

--- Game/Scripting/Action.cs
+++ Game/Scripting/Action.cs
@@ -1,9 +1,13 @@
 using System;
+using cse210_finalProject.Game.Casting;
 
 namespace cse210_finalProject.Game.Scripting
 {
     public class Action
     {
-        
+        public virtual void Execute(Cast cast, Script script)
+        {
+            
+        }
     }
 }
